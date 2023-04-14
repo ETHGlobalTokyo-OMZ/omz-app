063601023b8ef0bfbe559c021a8461b6d04d0861
delivan <wjdgurdl272@gmail.com>
2023-04-14 22:14:19 +0900

    Add temp buy page

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,22 +1,65 @@
 /* eslint-disable react/button-has-type */
+/* eslint-disable jsx-a11y/control-has-associated-label */
 import type { NextPage } from 'next';
-import useCounter from 'hooks/useCounter';
-import dynamic from 'next/dynamic';
 
-const ConnectButton = dynamic(() => import('../components/ConnectButton'), {
-  ssr: false
-});
+const Home: NextPage = () => (
+  <div className="flex flex-col gap-5">
+    <div className="flex gap-2">
+      <h1>OTC market</h1>
+      <span>|</span>
+      <h2>Buy</h2>
+    </div>
+    <div>
+      <table className="w-full">
+        <tr className="bg-[#22252E]">
+          <th className="py-3 px-4 text-left">Token</th>
+          <th className="py-3 px-4">Price</th>
+          <th className="py-3 px-4">Deposit</th>
+          <th className="py-3 px-4">Expired</th>
+          <th className="py-3 px-4" />
+        </tr>
+        <tr>
+          <td className="p-4 text-left">ETH</td>
+          <td className="p-4 text-center">1000 BOB</td>
+          <td className="p-4 text-center">1 BOB</td>
+          <td className="p-4 text-center">2023.03.25 16:00</td>
+          <td className="p-4 text-center">
+            <button>Buy</button>
+          </td>
+        </tr>
 
-const Home: NextPage = () => {
-  const { count, incrementCount } = useCounter();
-  return (
-    <>
-      <h1>Count</h1>
-      <div>{count}</div>
-      <button onClick={incrementCount}>Increment</button>
-      <ConnectButton />
-    </>
-  );
-};
+        <tr>
+          <td className="p-4 text-left">ETH</td>
+          <td className="p-4 text-center">1000 BOB</td>
+          <td className="p-4 text-center">1 BOB</td>
+          <td className="p-4 text-center">2023.03.25 16:00</td>
+          <td className="p-4 text-center">
+            <button>Buy</button>
+          </td>
+        </tr>
+
+        <tr>
+          <td className="p-4 text-left">ETH</td>
+          <td className="p-4 text-center">1000 BOB</td>
+          <td className="p-4 text-center">1 BOB</td>
+          <td className="p-4 text-center">2023.03.25 16:00</td>
+          <td className="p-4 text-center">
+            <button>Buy</button>
+          </td>
+        </tr>
+
+        <tr>
+          <td className="p-4 text-left">ETH</td>
+          <td className="p-4 text-center">1000 BOB</td>
+          <td className="p-4 text-center">1 BOB</td>
+          <td className="p-4 text-center">2023.03.25 16:00</td>
+          <td className="p-4 text-center">
+            <button>Buy</button>
+          </td>
+        </tr>
+      </table>
+    </div>
+  </div>
+);
 
 export default Home;
